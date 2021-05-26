8d510250374c26c56b3c740e5f2c4a9278d53c3f
Liu Hui Zhi Fen Si <2363918549@qq.com>
2021-05-26 21:10:29 +0800

    added solitaire demo

---
+++ solitaire/src/settings.bca45.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/SolitaireDemo.fire",orientation:"",jsList:[],bundleVers:{internal:"69b9a",main:"b739f"}};
