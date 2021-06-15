c6012d4e90453ee775199686b65842de8183282c
Liu Hui Zhi Fen Si <2363918549@qq.com>
2021-06-15 16:53:11 +0800

    fix

--- solitaire1_1/src/settings.24ee8.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/SolitaireDemo.fire",orientation:"",jsList:[],bundleVers:{internal:"69b9a",main:"18cc0"}};
